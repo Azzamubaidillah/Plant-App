8ae8f3a667743364a13007a0656f17b025a34574
Azzam Ubaidillah <azzam.ubaidillah05@gmail.com>
2021-12-31 02:17:02 +0700

    featured plants component

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -1,10 +1,10 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, deprecated_member_use
 
 import 'package:flutter/material.dart';
-import 'package:plant_application/constants.dart';
 import 'package:plant_application/screens/home/components/recomends_plants.dart';
 import 'package:plant_application/screens/home/components/title_with_more_button.dart';
 
+import 'featured_plants.dart';
 import 'header_with_searchbox.dart';
 
 class Body extends StatelessWidget {
@@ -22,6 +22,11 @@ class Body extends StatelessWidget {
             press: () {},
           ),
           RecomendsPlants(),
+          TitleWithMoreButton(
+            title: "Featured Plants",
+            press: () {},
+          ),
+          FeaturedPlants(),
         ],
       ),
     );
