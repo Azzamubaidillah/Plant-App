c7b5b06ed033362d194e642c0750ee5c1be0a124
Azzam Ubaidillah <azzam.ubaidillah05@gmail.com>
2021-12-31 01:54:06 +0700

    extract widget RecommendPlantRecommendation

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -20,59 +20,94 @@ class Body extends StatelessWidget {
             title: "Recommended",
             press: () {},
           ),
-          Container(
-            margin: EdgeInsets.only(
-              left: kDefaultPadding,
-              top: kDefaultPadding / 2,
-              bottom: kDefaultPadding * 2.5,
-            ),
-            // 40% total width
-            width: size.width * 0.4,
-            child: Column(
-              children: <Widget>[
-                Image.asset("assets/images/image_1.png"),
-                Container(
-                  padding: EdgeInsets.all(kDefaultPadding / 2),
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    boxShadow: [
-                      BoxShadow(
-                        offset: Offset(0, 10),
-                        blurRadius: 50,
-                        color: kPrimaryColor.withOpacity(0.23),
-                      ),
-                    ],
-                  ),
-                  child: Row(
-                    children: [
-                      RichText(
-                        text: TextSpan(
-                          children: [
-                            TextSpan(
-                              text: "Samantha\n".toUpperCase(),
-                              style: Theme.of(context).textTheme.button,
-                            ),
-                            TextSpan(
-                              text: "Russia".toUpperCase(),
-                              style: TextStyle(
-                                color: kPrimaryColor.withOpacity(0.5),
-                              ),
-                            )
-                          ],
-                        ),
-                      ),
-                      Spacer(),
-                      Text(
-                        '\$440',
-                        style: Theme.of(context)
-                            .textTheme
-                            .button!
-                            .copyWith(color: kPrimaryColor),
-                      )
-                    ],
-                  ),
+          RecommendPlantRecommendation(
+            image: "assets/images/image_1.png",
+            title: "Samantha",
+            country: "Russia",
+            price: 440,
+            press: () {},
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class RecommendPlantRecommendation extends StatelessWidget {
+  const RecommendPlantRecommendation({
+    Key? key,
+    required this.image,
+    required this.title,
+    required this.country,
+    required this.price,
+    required this.press,
+  }) : super(key: key);
+
+  final String image, title, country;
+  final int price;
+  final VoidCallback press;
+
+  @override
+  Widget build(BuildContext context) {
+    final Size size = MediaQuery.of(context).size;
+
+    return Container(
+      margin: EdgeInsets.only(
+        left: kDefaultPadding,
+        top: kDefaultPadding / 2,
+        bottom: kDefaultPadding * 2.5,
+      ),
+      // 40% total width
+      width: size.width * 0.4,
+      child: Column(
+        children: <Widget>[
+          Image.asset(image),
+          GestureDetector(
+            onTap: press,
+            child: Container(
+              padding: EdgeInsets.all(kDefaultPadding / 2),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(10),
+                  bottomRight: Radius.circular(10),
                 ),
-              ],
+                color: Colors.white,
+                boxShadow: [
+                  BoxShadow(
+                    offset: Offset(0, 10),
+                    blurRadius: 50,
+                    color: kPrimaryColor.withOpacity(0.23),
+                  ),
+                ],
+              ),
+              child: Row(
+                children: [
+                  RichText(
+                    text: TextSpan(
+                      children: [
+                        TextSpan(
+                          text: "$title\n".toUpperCase(),
+                          style: Theme.of(context).textTheme.button,
+                        ),
+                        TextSpan(
+                          text: country.toUpperCase(),
+                          style: TextStyle(
+                            color: kPrimaryColor.withOpacity(0.5),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  Spacer(),
+                  Text(
+                    '\$$price',
+                    style: Theme.of(context)
+                        .textTheme
+                        .button!
+                        .copyWith(color: kPrimaryColor),
+                  )
+                ],
+              ),
             ),
           ),
         ],
