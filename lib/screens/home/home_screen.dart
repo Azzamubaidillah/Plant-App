dd39fcb588add81c38b53785c265ec8a10286461
Azzam Ubaidillah <azzam.ubaidillah05@gmail.com>
2022-01-01 00:47:46 +0700

    move file bottom navbar

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -3,6 +3,8 @@ import 'package:flutter_svg/svg.dart';
 import 'package:plant_application/constants.dart';
 import 'package:plant_application/screens/home/components/body.dart';
 
+import 'components/bottom_nav_bar.dart';
+
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -20,50 +22,6 @@ class HomeScreen extends StatelessWidget {
       leading: IconButton(
         icon: SvgPicture.asset("assets/icons/menu.svg"),
         onPressed: () {},
-      ),
-    );
-  }
-}
-
-class BottomNavBar extends StatelessWidget {
-  const BottomNavBar({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.only(
-        left: kDefaultPadding * 2,
-        right: kDefaultPadding * 2,
-      ),
-      height: 60,
-      decoration: BoxDecoration(
-        color: Colors.white,
-        boxShadow: [
-          BoxShadow(
-            offset: Offset(0, -10),
-            blurRadius: 35,
-            color: kPrimaryColor.withOpacity(0.38),
-          ),
-        ],
-      ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          IconButton(
-            onPressed: () {},
-            icon: SvgPicture.asset("assets/icons/flower.svg"),
-          ),
-          IconButton(
-            onPressed: () {},
-            icon: SvgPicture.asset("assets/icons/heart-icon.svg"),
-          ),
-          IconButton(
-            onPressed: () {},
-            icon: SvgPicture.asset("assets/icons/user-icon.svg"),
-          ),
-        ],
       ),
     );
   }
