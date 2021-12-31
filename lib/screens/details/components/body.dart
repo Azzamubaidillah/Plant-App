98e829f71afae456a4d73281475fdc5387201646
Azzam Ubaidillah <azzam.ubaidillah05@gmail.com>
2022-01-01 03:51:29 +0700

    back button and image

--- lib/screens/details/components/body.dart
+++ lib/screens/details/components/body.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:plant_application/constants.dart';
 
 class Body extends StatelessWidget {
@@ -8,35 +9,59 @@ class Body extends StatelessWidget {
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Column(
-      children: [
-        Row(
-          children: [
-            Expanded(
-              child: Column(),
-            ),
-            Container(
-              height: size.height * 0.8,
-              width: size.width * 0.75,
-              decoration: BoxDecoration(
-                borderRadius: const BorderRadius.only(
-                  topLeft: Radius.circular(63),
-                  bottomLeft: Radius.circular(63),
-                ),
-                boxShadow: [
-                  BoxShadow(
-                      offset: Offset(0, 10),
-                      blurRadius: 60,
-                      color: kPrimaryColor.withOpacity(0.28)),
-                ],
-                image: const DecorationImage(
-                  alignment: Alignment.centerLeft,
-                  fit: BoxFit.cover,
-                  image: AssetImage("assets/images/img.png"),
+      children: <Widget>[
+        SizedBox(
+          height: size.height * 0.8,
+          child: Row(
+            children: <Widget>[
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                    vertical: kDefaultPadding * 3,
+                  ),
+                  child: Column(
+                    children: [
+                      Align(
+                        alignment: Alignment.topLeft,
+                        child: IconButton(
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: kDefaultPadding,
+                          ),
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                          icon: SvgPicture.asset("assets/icons/back_arrow.svg"),
+                        ),
+                      ),
+                      const Spacer(),
+                    ],
+                  ),
                 ),
               ),
-            )
-          ],
-        )
+              Container(
+                height: size.height * 0.8,
+                width: size.width * 0.75,
+                decoration: BoxDecoration(
+                  borderRadius: const BorderRadius.only(
+                    topLeft: Radius.circular(63),
+                    bottomLeft: Radius.circular(63),
+                  ),
+                  boxShadow: [
+                    BoxShadow(
+                        offset: const Offset(0, 10),
+                        blurRadius: 60,
+                        color: kPrimaryColor.withOpacity(0.28)),
+                  ],
+                  image: const DecorationImage(
+                    alignment: Alignment.centerLeft,
+                    fit: BoxFit.cover,
+                    image: AssetImage("assets/images/img.png"),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
       ],
     );
   }
