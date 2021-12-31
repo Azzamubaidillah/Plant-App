124b9348fc47bf8476da63b0416059dfc744e98a
Azzam Ubaidillah <azzam.ubaidillah05@gmail.com>
2022-01-01 00:41:41 +0700

    bottom navbar

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -9,6 +9,7 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       appBar: buildAppBar(),
       body: Body(),
+      bottomNavigationBar: BottomNavBar(),
     );
   }
 
@@ -19,6 +20,50 @@ class HomeScreen extends StatelessWidget {
       leading: IconButton(
         icon: SvgPicture.asset("assets/icons/menu.svg"),
         onPressed: () {},
+      ),
+    );
+  }
+}
+
+class BottomNavBar extends StatelessWidget {
+  const BottomNavBar({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.only(
+        left: kDefaultPadding * 2,
+        right: kDefaultPadding * 2,
+      ),
+      height: 60,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(
+            offset: Offset(0, -10),
+            blurRadius: 35,
+            color: kPrimaryColor.withOpacity(0.38),
+          ),
+        ],
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          IconButton(
+            onPressed: () {},
+            icon: SvgPicture.asset("assets/icons/flower.svg"),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: SvgPicture.asset("assets/icons/heart-icon.svg"),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: SvgPicture.asset("assets/icons/user-icon.svg"),
+          ),
+        ],
       ),
     );
   }
