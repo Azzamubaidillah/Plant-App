664c97c0975b086716c57519a57ab4def24360c2
Azzam Ubaidillah <azzam.ubaidillah05@gmail.com>
2022-01-01 13:12:31 +0700

    title and price

--- lib/screens/details/components/body.dart
+++ lib/screens/details/components/body.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
-import 'package:plant_application/constants.dart';
 
-import 'icon_card.dart';
+import 'image_and_icons.dart';
+import 'title_and_price.dart';
 
 class Body extends StatelessWidget {
   const Body({Key? key}) : super(key: key);
@@ -10,65 +9,17 @@ class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    return Column(
-      children: <Widget>[
-        SizedBox(
-          height: size.height * 0.8,
-          child: Row(
-            children: <Widget>[
-              Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(
-                    vertical: kDefaultPadding * 3,
-                  ),
-                  child: Column(
-                    children: [
-                      Align(
-                        alignment: Alignment.topLeft,
-                        child: IconButton(
-                          padding: const EdgeInsets.symmetric(
-                            horizontal: kDefaultPadding,
-                          ),
-                          onPressed: () {
-                            Navigator.pop(context);
-                          },
-                          icon: SvgPicture.asset("assets/icons/back_arrow.svg"),
-                        ),
-                      ),
-                      const Spacer(),
-                      const IconCard(icon: "assets/icons/sun.svg"),
-                      const IconCard(icon: "assets/icons/icon_2.svg"),
-                      const IconCard(icon: "assets/icons/icon_3.svg"),
-                      const IconCard(icon: "assets/icons/icon_4.svg"),
-                    ],
-                  ),
-                ),
-              ),
-              Container(
-                height: size.height * 0.8,
-                width: size.width * 0.75,
-                decoration: BoxDecoration(
-                  borderRadius: const BorderRadius.only(
-                    topLeft: Radius.circular(63),
-                    bottomLeft: Radius.circular(63),
-                  ),
-                  boxShadow: [
-                    BoxShadow(
-                        offset: const Offset(0, 10),
-                        blurRadius: 60,
-                        color: kPrimaryColor.withOpacity(0.28)),
-                  ],
-                  image: const DecorationImage(
-                    alignment: Alignment.centerLeft,
-                    fit: BoxFit.cover,
-                    image: AssetImage("assets/images/img.png"),
-                  ),
-                ),
-              )
-            ],
+    return SingleChildScrollView(
+      child: Column(
+        children: <Widget>[
+          ImageAndIcons(size: size),
+          const TitleAndPrice(
+            title: "Angelica",
+            country: "Russia",
+            price: 440,
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
