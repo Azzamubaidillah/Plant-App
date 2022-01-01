a83ea93f57107506de06c677c0e0b09347160792
Azzam Ubaidillah <azzam.ubaidillah05@gmail.com>
2022-01-02 00:22:20 +0700

    asset auth

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 const kPrimaryColor = Color(0xFF006994);
+const kPrimaryLightColor = Color(0xFFC7F8F9);
 const kTextColor = Color(0xFF3C4046);
 const kBackgroundColor = Color(0xFFF9F8FD);
 
